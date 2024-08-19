7c924396d6564bb5c6d87ea3c49ae2de0a4cfc12
pdxlover <jieunlee0603@gmail.com>
2024-08-19 11:17:11 +0900

    레이어팝업
    레이어팝업

--- js/common.js
+++ js/common.js
@@ -1,6 +1,40 @@
 
 
 $(document).ready(function () {//시작
+
+  //루나셀 정품 레이어팝업 동작
+  // 쿠키 생성
+function setCookie( name, value, expiredays ) {  // 쿠키저장
+	var todayDate = new Date();  //date객체 생성 후 변수에 저장
+	todayDate.setDate( todayDate.getDate() + expiredays ); 
+   	 // 시간지정(현재시간 + 지정시간)
+	document.cookie = name + "=" + value + "; path=/; expires=" + todayDate.toUTCString() + ";"
+	//위 정보를 쿠키에 굽는다
+} 
+
+//하루동안 팝업창 닫기
+$(function(){
+	$(".popup_box").draggable({containment:'parent', scroll:false}); // 레이어 팝업 창 드래그 가능
+	//{containment:'parent', scroll:false} 화면 영역 밖으로 드래그 안됌.
+                
+	if(document.cookie.indexOf("popToday=close") < 0 ){      // 쿠키 저장여부 체크
+		document.getElementById("popup_layer").style.display = "block";
+		}else {
+		document.getElementById("popup_layer").style.display = "none"; 
+		}
+	});
+             
+//오늘하루만보기 닫기버튼 스크립트
+function closeToday() { 
+	setCookie( "popToday", "close" , 1  ); 
+	$("#popup_layer").css("display", "none");
+	document.getElementById("popup_layer").style.display = "none";
+}
+//그냥 닫기버튼 스크립트
+function closePop() { 
+	document.getElementById("popup_layer").style.display = "none";
+}
+
   
 
   //모바일 메뉴 열고 닫기
